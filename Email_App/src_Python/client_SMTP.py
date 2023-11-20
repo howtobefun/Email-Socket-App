61a7cc2760f6ee9c4cf6028f531877a540eb350b
howtobefun <duytdn2806@gmail.com>
2023-11-20 09:13:14 +0700

    refactor some client_SMTP code before fixing bugs

--- Email_App/src_Python/client_SMTP.py
+++ Email_App/src_Python/client_SMTP.py
@@ -4,7 +4,7 @@ class Client_SMTP:
     def __init__(self, mailserver, port, username):
         self.mailserver = mailserver
         self.port = port
-        self.username = "<" + username + ">"
+        self.username = username
 
         self.clientSocket = socket.socket()
         self.clientSocket.connect((mailserver, port))
@@ -17,34 +17,34 @@ class Client_SMTP:
 
     def sendEmail(self, recipient):
         self.__command_MAIL_FROM()
-        self.__command_RCPT_TO("<" + recipient + ">")
+        self.__command_RCPT_TO(recipient)
         self.__command_DATA("Anh DuyTech")
-    
+
     def endSession(self):
         self.__command_QUIT()
         self.clientSocket.close()
 
     def __command_HELO(self):
-        heloCommand = f'EHLO [{self.mailserver}]\r\n'
-        self.clientSocket.send(heloCommand.encode())
+        command = f'EHLO [{self.mailserver}]\r\n'
+        self.clientSocket.send(command.encode())
         recv = self.clientSocket.recv(1024).decode()
         print(recv)
 
     def __command_MAIL_FROM(self):
-        mailFromCommand = f"MAIL FROM: {self.username}\r\n"
-        self.clientSocket.send(mailFromCommand.encode())
+        command = f"MAIL FROM: <{self.username}>\r\n"
+        self.clientSocket.send(command.encode())
         recv = self.clientSocket.recv(1024).decode()
         print(recv)
 	
     def __command_RCPT_TO(self, recipient):
-        rcptToCommand = f"RCPT TO: {recipient}\r\n"
-        self.clientSocket.send(rcptToCommand.encode())
+        command = f"RCPT TO: <{recipient}>\r\n"
+        self.clientSocket.send(command.encode())
         recv = self.clientSocket.recv(1024).decode()
         print(recv)
 	
     def __command_DATA(self, msg):
-        dataCommand = "DATA\r\n"
-        self.clientSocket.send(dataCommand.encode())
+        command = "DATA\r\n"
+        self.clientSocket.send(command.encode())
         recv = self.clientSocket.recv(1024).decode()
         print(recv)
         if (recv[:3] == '354'):
@@ -53,6 +53,6 @@ class Client_SMTP:
             self.clientSocket.recv(1024)
 
     def __command_QUIT(self):
-        quit_command = "QUIT\r\n"
-        self.clientSocket.send(quit_command.encode())
+        command = "QUIT\r\n"
+        self.clientSocket.send(command.encode())
         self.clientSocket.recv(1024)
