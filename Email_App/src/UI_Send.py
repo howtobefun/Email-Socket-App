1ed0e5dab3f570d45549d407d67b70f221c76699
Phuc0210 <vtphuc22@clc.fitus.edu.vn>
2023-11-27 14:09:55 +0700

    pick file

--- Email_App/src/UI_Send.py
+++ Email_App/src/UI_Send.py
@@ -2,7 +2,7 @@ import flet as ft
 from UI_User import *
 
 def SendPage(page: ft.Page):
-    user = User()
+   # user = User()
 
     to = ft.TextField(label="To", width=500)
     cc = ft.TextField(label="Cc", multiline=True, width=500)
@@ -14,22 +14,38 @@ def SendPage(page: ft.Page):
         if(to.value):
             page.add(ft.Row([ft.Text(value="Sending successfully to "+ to.value,size=10)],
                             alignment=ft.MainAxisAlignment.CENTER))
-            user.SMTPclient.sendEmail(
-                mailTo_str= to.value,
-                cc_str= cc.value,
-                bcc_str= bcc.value,
-                subject= subject.value,
-                content= content.value
-            )
+            # user.SMTPclient.sendEmail(
+            #     mailTo_str= to.value,
+            #     cc_str= cc.value,
+            #     bcc_str= bcc.value,
+            #     subject= subject.value,
+            #     content= content.value
+            # )
             
 
     sendButton=ft.ElevatedButton(text="Send",on_click=send)
-   
+
+    fname=" "
+    fileName=ft.Text(value=fname)
+    def showPickFile(e: ft.FilePickerResultEvent):
+        for x in e.files:
+            global fname
+            fname=fname+x.name
+            fileName.value=fname
+            page.update()
+
+    file_picker = ft.FilePicker(on_result=showPickFile)
+    page.overlay.append(file_picker)
+
+
     page.add(
           ft.Column(
             [
-                to,cc,bcc,
+               # to,cc,bcc,
                 subject,
+                ft.ElevatedButton("Choose files...",
+                    on_click=lambda _: file_picker.pick_files(allow_multiple=True)),
+                fileName,
                 content
             ],
             alignment=ft.MainAxisAlignment.START
@@ -39,4 +55,5 @@ def SendPage(page: ft.Page):
      
      
 
-    
+if __name__ == "__main__":
+    ft.app(target=SendPage)
