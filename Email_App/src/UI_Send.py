f4b935223c66785a2492be3f51286a95e7950637
Phuc0210 <vtphuc22@clc.fitus.edu.vn>
2023-11-27 15:13:50 +0700

    pick file but not delete file

--- Email_App/src/UI_Send.py
+++ Email_App/src/UI_Send.py
@@ -2,46 +2,47 @@ import flet as ft
 from UI_User import *
 
 def SendPage(page: ft.Page):
-   # user = User()
+    user = User()
 
-    to = ft.TextField(label="To", width=500)
-    cc = ft.TextField(label="Cc", multiline=True, width=500)
-    bcc = ft.TextField(label="Bcc", multiline=True, width=500)
-    subject = ft.TextField(label="Subject", multiline=True)
-    content= ft.TextField(label="Content", multiline=True,min_lines=12)
+    to = ft.TextField(label="To",height=40, width=500)
+    cc = ft.TextField(label="Cc", height=40,width=500)
+    bcc = ft.TextField(label="Bcc", height=40, width=500)
+    subject = ft.TextField(label="Subject", height=40,)
+    content= ft.TextField(label="Content", min_lines=12)
 
     def send(e):#e để nhận tín hiệu từ nút
         if(to.value):
             page.add(ft.Row([ft.Text(value="Sending successfully to "+ to.value,size=10)],
                             alignment=ft.MainAxisAlignment.CENTER))
-            # user.SMTPclient.sendEmail(
-            #     mailTo_str= to.value,
-            #     cc_str= cc.value,
-            #     bcc_str= bcc.value,
-            #     subject= subject.value,
-            #     content= content.value
-            # )
+            user.SMTPclient.sendEmail(
+                mailTo_str= to.value,
+                cc_str= cc.value,
+                bcc_str= bcc.value,
+                subject= subject.value,
+                content= content.value
+            )
             
 
     sendButton=ft.ElevatedButton(text="Send",on_click=send)
 
-    fname=" "
-    fileName=ft.Text(value=fname)
+    fileName=ft.Text("")
     def showPickFile(e: ft.FilePickerResultEvent):
         for x in e.files:
-            nonlocal fname
-            fname=fname+x.name
-            fileName.value=fname
+            fileName.value=fileName.value+" \""+x.name+"\""
             page.update()
 
     file_picker = ft.FilePicker(on_result=showPickFile)
     page.overlay.append(file_picker)
-
+    
+    # def doSomethingWithPickFile():
+    #     if file_picker.result != None and file_picker.result.files != None:
+    #         for f in file_picker.result.files:
+    #             pass (tác động lên f)
 
     page.add(
           ft.Column(
             [
-               # to,cc,bcc,
+                to,cc,bcc,
                 subject,
                 ft.ElevatedButton("Choose files...",
                     on_click=lambda _: file_picker.pick_files(allow_multiple=True)),
