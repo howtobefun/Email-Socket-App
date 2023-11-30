2a459fd757ff96bdfd120893418d08f937a73a8e
Phuc0210 <vtphuc22@clc.fitus.edu.vn>
2023-11-30 22:58:15 +0700

    complete OOP send

--- Email_App/src/UI/UI_Send.py
+++ Email_App/src/UI/UI_Send.py
@@ -18,7 +18,7 @@ class PickFileSystem(ft.UserControl):
     def build(self):
         self.fileBar = ft.Row()
         self.filePicker = ft.FilePicker(on_result=self.addFile)
-        self.filePath=ft.Text("")
+        self.filePath=[]
         self.page.overlay.append(self.filePicker)
 
         return ft.Column(
@@ -31,9 +31,7 @@ class PickFileSystem(ft.UserControl):
     
     def findControlByPath(self,path: str):
             for control in self.fileBar.controls:
-                print("Falseeeeee")
                 if control.key == path:
-                    print("Trueeee")
                     return control
             return None
 
@@ -41,7 +39,7 @@ class PickFileSystem(ft.UserControl):
         controlToDelete = self.findControlByPath(fileContainerComponent.filePath)
         if (controlToDelete == None):
             return
-        self.filePath.value.replace(fileContainerComponent.filePath,"")
+        self.filePath.remove(fileContainerComponent.filePath)
         self.fileBar.controls.remove(controlToDelete)
         del fileContainerComponent
         self.update()
@@ -57,12 +55,13 @@ class PickFileSystem(ft.UserControl):
                                                     ft.icons.DELETE,
                                                     on_click=fileContainerComponent.remove_file_from_list
                                             )
-                ]
-            ),
-                            bgcolor=ft.colors.BLUE_100
+                                        ]
+                            ),
+                            bgcolor=ft.colors.BLUE_100,
+                            key=x.path
             )
             self.fileBar.controls.append(filePicked)
-            self.filePath.value = self.filePath.value+" "+x.path
+            self.filePath.append(x.path)
             self.update()
 
 
@@ -75,7 +74,7 @@ def SendPage(page: ft.Page):
     subject = ft.TextField(label="Subject", height=40,)
     content= ft.TextField(label="Content", min_lines=12)
 
-    def send(e):#e để nhận tín hiệu từ nút
+    def send(e):
         if(to.value):
             page.add(ft.Row([ft.Text(value="Sending successfully to "+ to.value,size=10)],
                             alignment=ft.MainAxisAlignment.CENTER))
@@ -85,55 +84,18 @@ def SendPage(page: ft.Page):
                 bcc_str= bcc.value,
                 subject= subject.value,
                 content= content.value,
-                attachments= filePath.value
+                attachments= ", ".join(file.filePath)
             )
     sendButton=ft.ElevatedButton(text="Send",on_click=send)
-
-
-    fileBar=ft.Row()
-    filePath=ft.Text("")
-
-    # def deleteFile(self,e):
-    #     index=self.fileBar.index()
-    #     del self.fileBar.controls
-
-
-
-    def showPickFile(e: ft.FilePickerResultEvent):
-        for x in e.files:    
-            filePicked=ft.Container(
-            content=ft.Row([ft.Text(value=x.name),
-                            ft.IconButton(
-                                ft.icons.DELETE,
-                                on_click=deleteFile
-                                )
-                            ]
-                        ),
-            bgcolor=ft.colors.BLUE_100)
-            fileBar.controls.append(filePicked)
-            filePath.value = filePath.value+" "+x.path
-            page.update()
-
-    #file_picker = ft.FilePicker(on_result=showPickFile)
-    #page.overlay.append(file_picker)
     
-    p = PickFileSystem(page) 
-
-
-    # def doSomethingWithPickFile():
-    #     if file_picker.result != None and file_picker.result.files != None:
-    #         for f in file_picker.result.files:
-    #             pass (tác động lên f)
+    file = PickFileSystem(page) 
 
     page.add(
           ft.Column(
             [
                 to,cc,bcc,
                 subject,
-                # ft.ElevatedButton("Choose files",
-                #     on_click=lambda _: file_picker.pick_files(allow_multiple=True)),
-                # fileBar,
-                p,
+                file,
                 content
             ],
             alignment=ft.MainAxisAlignment.START
