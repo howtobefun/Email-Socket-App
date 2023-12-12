90423f16f429a1e498a18864eeeb0126c8605bbf
howtobefun <duytdn2806@gmail.com>
2023-12-12 13:56:13 +0700

    Added read_status_handling.py

---
+++ Email_App/src/client/read_status_handling.py
@@ -0,0 +1,40 @@
+import json
+import os
+
+class ReadStatusHandling:
+    def __init__(self, email: str):
+        self.email = email
+        self.USERS_MAILBOX = "User_Mailbox/"
+        self.USER_MAILBOX_PATH = self.USERS_MAILBOX + self.email + "/"
+        self.read_status_file_path = self.USER_MAILBOX_PATH + "read_status.json"
+
+        self.data = {}
+
+        if not os.path.exists(self.read_status_file_path):
+            self.create_read_status_file()
+        else:
+            with open(self.read_status_file_path, 'r') as fp:
+                self.data = json.load(fp)
+
+    def __generate_data(self):
+        data = {}
+        try: 
+            dirs = os.listdir(self.USER_MAILBOX_PATH)
+        except:
+            return data
+        
+        for dir in dirs:
+            if os.path.isfile(self.USER_MAILBOX_PATH + dir) | dir == 'Attachments':  
+                continue
+            msg_folders = os.listdir(self.USER_MAILBOX_PATH + dir)
+            for msg_folder in msg_folders:
+                msg_files = os.listdir(self.USER_MAILBOX_PATH + dir + "/" + msg_folder)
+                for msg_file in msg_files:
+                    data[msg_file] = False
+
+        return data
+
+    def create_read_status_file(self):
+        self.data = self.__generate_data()
+        with open(self.read_status_file_path, 'w') as fp:
+            json.dump(self.data, fp)
